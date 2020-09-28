3e19060fe7609fe0ac17ce5559c61bd953b065b7
Patricio Guzman <patricio.guzman@happn.fr>
2020-09-28 10:36:11 +0200

    🚀 Setup project and add gitignore

---
+++ KEDapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KEDapp
+//
+//  Created by Patricio Guzman on 28/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KEDapp/KEDappApp.swift
@@ -0,0 +1,17 @@
+//
+//  KEDappApp.swift
+//  KEDapp
+//
+//  Created by Patricio Guzman on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct KEDappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
